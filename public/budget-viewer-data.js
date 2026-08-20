f9d3854dd394eeceeabf444f43be6a6faebb37bd
Steven Jazziel <stevanjazziel@gmail.com>
2026-08-20 11:52:12 -0500

    chore: sync financiero exports

--- public/budget-viewer-data.js
+++ public/budget-viewer-data.js
@@ -2,7 +2,7 @@ window.BUDGET_VIEWER_PAYLOAD = {
   "meta": {
     "title": "Direcciones - Monto codificado 2026",
     "year": 2026,
-    "updatedAt": "2026-08-20T16:44:54.661Z"
+    "updatedAt": "2026-08-20T16:52:12.308Z"
   },
   "items": [
     {
